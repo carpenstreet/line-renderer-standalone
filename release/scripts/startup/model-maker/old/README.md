832cb3775ece6ed9716a2b0c377a0292bbcf87f3
Youdiie <youjin@acon3d.com>
2023-01-18 11:51:46 +0900

    모델메이커에서 쓰는 에셋들 삭제

--- release/scripts/startup/model-maker/old/README.md
+++
@@ -1,2 +0,0 @@
-# Introduction 
-Blender Geometry-Nodes를 이용한 모델링 자동화 메이커입니다. 살려주세요
--- release/scripts/startup/model-maker/old/nodemake/lib_FTP/utils_ftp.py
+++
@@ -1,5 +0,0 @@
-def ftp_open(filename, Download_Path, ftp):
-
-    with open(Download_Path + filename, "wb") as fd:
-        ftp.retrbinary("RETR " + filename, fd.write)
-        ftp.set_pasv(False)
